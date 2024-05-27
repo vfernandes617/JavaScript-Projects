22787e09e85e410086b89feb070583a8c98dc9ff
vfernandes617 <167803670+vfernandes617@users.noreply.github.com>
2024-05-27 13:02:30 -0400

    javascript assignment

---
+++ BasicJavaScriptProjects/Project1_expressions_alert/JS/Main.js
@@ -0,0 +1,18 @@
+   // Declare and assign a string variable
+   var message = "This is my alert"
+
+   // Use window.alert() to display the variable
+   window.alert(message);
+
+   document.write("Lisa told Bart, \"knock it off, Bart! Or I'll tell dad!\"<br> \"Eat my shorts!\" Bart responded.");
+
+   document.write("\To be or not to be,"+"Tubi or not Tubi?\"" +"ValterFernandes");
+   var Family = "The Fernandes", Dad="Valter", Mom="Thalia",Son="Marcelo",Daughter="Celia";
+   document.write=(Dad);
+//this is an expression below//
+   document.write(4+4);
+//my fuction displayed in the LMS//
+   function My_First_Function() {
+    var str="This is the button text!";
+    document.getElementById("Button_text").innerHTML=str;
+   }
