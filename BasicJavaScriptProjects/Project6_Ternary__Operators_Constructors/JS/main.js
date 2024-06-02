8d076dfa525405d70a2ffe36b2072ea226423cae
vfernandes617 <167803670+vfernandes617@users.noreply.github.com>
2024-06-01 20:26:58 -0400

    update

--- BasicJavaScriptProjects/Project6_Ternary__Operators_Constructors/JS/main.js
+++ BasicJavaScriptProjects/Project6_Ternary__Operators_Constructors/JS/main.js
@@ -17,7 +17,7 @@ function Ride_Function() {
     }
     var Jack=new Vehicle("Dodge","Viper",2020,"Red");
     var Emily=new Vehicle("Jeep","Trail Hawk",2019,"White and Black");
-    var Erik=new Vehicle("Ford","Pinto"1971,"Mustard");
+    var Erik=new Vehicle("Ford","Pinto",1971,"Mustard");
     function myFunction(){
         document.getElementById("Keywords_and_Constructors").innerHTML="Erik drives a"+Erik.Vehicle_Color+"-colored"+Erik.Vehicle_Model+
         "manufactured in"+Erik.Vehicle_Year;
